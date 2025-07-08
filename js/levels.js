742b4c1c30b76d6a16f693972c09b86fcdeb0238
Askinga <musicgemca@gmail.com>
2025-07-08 18:02:01 -0400

    Update levels.js

--- js/levels.js
+++ js/levels.js
@@ -1,4 +1,4 @@
-addLayer("L", {
+addLayer("l", {
     name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "L", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
@@ -22,7 +22,7 @@ addLayer("L", {
     },
     row: 1, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "l", description: "l: Reset for levels", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "l", description: "L: Reset for levels", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return (hasUpgrade('p', 45) || player.l.unlocked)},
     branches: ["p"],
