d5cd407b49e85e22b27ba7aa560548c32622569e
Askinga <musicgemca@gmail.com>
2025-07-09 19:08:10 -0400

    Update levels.js

--- js/levels.js
+++ js/levels.js
@@ -15,6 +15,7 @@ addLayer("l", {
     exponent: 1.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
+	if (hasUpgrade("s", 13)) mult = mult.times(1000)
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
