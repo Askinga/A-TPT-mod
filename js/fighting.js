4ea830f9b17457f6e84e083201b558373a64465f
Askinga <musicgemca@gmail.com>
2025-07-13 19:07:25 -0400

    Tried to fix Requirement not changing

--- js/fighting.js
+++ js/fighting.js
@@ -19,9 +19,9 @@ addLayer("f", {
     resetsNothing(){ return true },
     color: "#cc0000",
     requires() {
-	let r = new Decimal("e30");
-	if (player.f.points.gte(1)) r = new Decimal("10^^e300");
-	return r;
+	let req = new Decimal("e30");
+	if (player.f.points.gte(1)) req = new Decimal("10^^e300");
+	return req;
     }, // Can be a function that takes requirement increases into account
     resource: "???", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
