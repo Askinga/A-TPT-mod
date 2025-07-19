81162110bb220c7796a9c6ec38fca941e63d7217
Askinga <musicgemca@gmail.com>
2025-07-19 17:59:43 -0400

    Update ultra.js

--- js/ultra.js
+++ js/ultra.js
@@ -3,7 +3,7 @@ addLayer("u", {
     symbol: "UP", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: -1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
-        unlocked: false,
+        unlocked: a,
 		points: new Decimal(0),
     }},
     color: "#7b63ff", 
@@ -25,5 +25,12 @@ addLayer("u", {
         {key: "u", description: "U: Reset for ultra prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return (hasUpgrade('f', 35) || player.u.unlocked)},
-    branches: ["l", "s"]
+    branches: ["l", "s"],
+    upgrades: {
+	11: {
+	  title: "Oh no...",
+	  desciption: "x10 points, prestige points and x5 super prestige points",
+	  cost: new Decimal(1)
+	},
+    },
 })
