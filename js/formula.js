6585045d69afd0819a99848770c5301620f12794
Askinga <musicgemca@gmail.com>
2025-07-22 15:33:00 -0400

    Update formula.js

--- js/formula.js
+++ js/formula.js
@@ -45,9 +45,9 @@ addLayer("fo", {
         "main-display",
         ["display-text", function() { 
     if (player.fo.stage.eq(0)) 
-        return "<h2>Formula Stage " + format(player.fo.stage) + "</h2><br>The formula is (x+1)^0.5, boosting points by x" + format(tmp.fo.stage1) + " where x is your f points";
+        return "<h2>Formula Stage " + format(player.fo.stage) + "</h2><br>The formula is (x+1)^0.5, boosting points by x" + format(tmp.fo.stage1) + "<br>where x is your f points";
     else 
-        return "<h2>Formula Stage " + format(player.fo.stage) + "</h2><br>The formula is (x+1)^y, boosting points by x" + format(tmp.fo.stage0) + " where x is your f points" + "<br>y = " + format(player.fo.y); }],     
+        return "<h2>Formula Stage " + format(player.fo.stage) + "</h2><br>The formula is (x+1)^y, boosting points by x" + format(tmp.fo.stage0) + "<br>where x is your f points" + "<br>y = " + format(player.fo.y); }],     
         "prestige-button",
         "clickables",
         "buyables",
