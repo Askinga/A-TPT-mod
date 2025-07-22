5c3da32cc6ffe97eec6f27a967d3953c1185e2d8
Askinga <musicgemca@gmail.com>
2025-07-22 10:44:06 -0400

    Update formula.js

--- js/formula.js
+++ js/formula.js
@@ -1,4 +1,4 @@
-addLayer("f", {
+addLayer("fo", {
     name: "formula", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "F", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
