5b82c8b364625313e8f503b843386b951cb1216f
Askinga <musicgemca@gmail.com>
2025-07-23 13:11:59 -0400

    Update formula.js

--- js/formula.js
+++ js/formula.js
@@ -178,7 +178,7 @@ addLayer("fo", {
             description: "Boost FP based on super prestige points",
             cost: new Decimal("100000"),
             unlocked(){ return (player.fo.stage.gte(2) && hasUpgrade('fo', 24)) },
-            effect(){ return player.s.points.add(1).pow(0.03) },
+            effect(){ return player.s.points.add(1).pow(0.0375) },
             effectDisplay(){ return "x"+format(upgradeEffect(this.layer, this.id)) },
         },
     },
