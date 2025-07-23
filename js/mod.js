def011562ff268bfeffb55d53ce24d52ed4a6785
Askinga <musicgemca@gmail.com>
2025-07-23 18:26:44 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -2,7 +2,7 @@ let modInfo = {
   name: "A TPT mod?",
   author: "Askinga/Sanas",
   pointsName: "points",
-  modFiles: ["layers.js", "tree.js", "automation.js", "levels.js", "super.js", "fighting.js", "ultra.js", "formula.js"],
+  modFiles: ["layers.js", "tree.js", "automation.js", "levels.js", "super.js", "fighting.js", "ultra.js", "formula.js", "boosters.js"],
 
   discordName: "",
   discordLink: "",
