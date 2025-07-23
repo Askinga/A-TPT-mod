a5607c56a08732630a9c31d9249f9d255540b61f
Askinga <musicgemca@gmail.com>
2025-07-23 15:35:47 -0400

    Update fighting.js

--- js/fighting.js
+++ js/fighting.js
@@ -290,7 +290,13 @@ addLayer("f", {
       if (player.f.enemyHP.lte(0)) {
         player.f.enemyHP = player.f.enemyStartHP
         player.f.coins = player.f.coins.add(player.f.coinGet)
+	if (player.a.auto8.eq(1)) {
+	    player.f.stage = player.f.stage.add(1)
+	}
       }
+      if (player.a.auto8.eq(1)) {
+	player.f.enemyHP = player.f.enemyHP.sub(player.f.damage.div(100).times(player.a.auto5).times(diff))
+      },
     },
     buyables: {
     11: {
