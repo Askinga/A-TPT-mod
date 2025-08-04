817ce074ec9fe7dfea2e331a202fd422050b935f
Askinga <musicgemca@gmail.com>
2025-08-03 20:14:48 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -67,6 +67,7 @@ function getPointGen() {
   if (player.fo.stage.eq(1)) gain = gain.times(tmp.fo.stage0)
   if (player.fo.stage.eq(2)) gain = gain.times(tmp.fo.stage2)
   if (player.fo.stage.eq(3)) gain = gain.times(tmp.fo.stage3)
+  if (hasUpgrade('i', 32)) gain = gain.pow(1.002)
   if (inChallenge('s', 12)) gain = gain.pow(0.5)
   return gain;
 }
