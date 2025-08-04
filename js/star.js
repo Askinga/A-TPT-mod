e49d19859df08c3dd594deca4214502dcf9bfa43
Askinga <musicgemca@gmail.com>
2025-08-04 16:02:39 -0400

    Update star.js

--- js/star.js
+++ js/star.js
@@ -1,4 +1,4 @@
-addLayer("star", {
+addLayer("st", {
     name: "Star", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "S", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
@@ -22,12 +22,12 @@ addLayer("star", {
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new Decimal(1)
     },
-    effect() { return player.star.points.add(1).pow(5) },
+    effect() { return player.st.points.add(1).pow(5) },
     row: 3, // Row the layer is in on the tree (0 is the first row)
     resetsNothing() { return true },
-    canReset() { return !player.star.unlocked },
+    canReset() { return !player.st.unlocked },
     tabFormat: [
-        ["display-text", function() { return "The star is " + format(player.star.points) + "km big, giving a x" + format(layers.star.effect()) + " boost to FP" }],
+        ["display-text", function() { return "The star is " + format(player.st.points) + "km big, giving a x" + format(layers.st.effect()) + " boost to FP" }],
         "blank",
         "clickables",
         "upgrades",
@@ -37,18 +37,18 @@ addLayer("star", {
         return Decimal.log10(player.points.add(1)).div(29000).pow(1.25)
     },
     branches: ["u", "f", "fo"],
-    layerShown() { return (hasUpgrade('i', 35) || player.star.unlocked) },
+    layerShown() { return (hasUpgrade('i', 35) || player.st.unlocked) },
     clickables: {
         11: {
             display() { return "Feed the star " + format(player.points) + " points." },
             onClick() { 
-                player.star.pointSize = tmp.star.Points
-                player.star.pointReq = player.points
+                player.st.pointSize = tmp.st.Points
+                player.st.pointReq = player.points
             },
-            canClick() { return player.points.gte(player.star.pointReq) },
+            canClick() { return player.points.gte(player.st.pointReq) },
         },
     },
     update(diff) {
-        player.star.points = player.star.pointSize
+        player.st.points = player.st.pointSize
     },
 })
