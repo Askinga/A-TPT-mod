056eae2340c903f96aca28f2e6bcd239377507c8
Askinga <musicgemca@gmail.com>
2025-08-20 16:48:35 -0400

    Update starB.js

--- js/starB.js
+++ js/starB.js
@@ -21,7 +21,7 @@ addLayer("stb", {
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new Decimal(1)
     },
-    row: 0, // Row the layer is in on the tree (0 is the first row)
+    row: 3, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
         {key: "t", description: "T: Reset for star boosters", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
