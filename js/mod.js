2d91691a27b9ee05fb3ac09e71cbf2989c53c4fe
Askinga <musicgemca@gmail.com>
2025-08-21 20:43:31 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -101,6 +101,7 @@ function getPointGen() {
   if (hasUpgrade("supernova", 25)) gain = gain.times(upgradeEffect("supernova", 25));
   if (hasUpgrade("supernova", 33)) gain = gain.times(upgradeEffect("supernova", 33));
   if (hasUpgrade('supernova', 34)) gain = gain.times("e25000")
+  if (hasUpgrade('supernova', 35)) gain = gain.times("e30000")
   if (inChallenge('s', 12)) gain = gain.pow(0.5)
   return gain;
 }
